2031e2f563d8338e2ecf9549ce7a23b1fcf62a50
MichelleDuque <michelled_1994@hotmail.com>
2023-10-06 03:15:26 -0400

    array for projects was added

--- js/main.js
+++ js/main.js
@@ -23,5 +23,20 @@
         tl.to(".text", {duration: 1, text: phrase})
         mainTL.add(tl)
     })
+    
+
+
+    //Projects Info
+
+    const projectParagraph = document.querySelector("#paragraph-project")
+
+    const projects = [
+        {name: "Mokepon", description: "Game created for platzi Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."},
+        {name: "Star Wars", description: "Star wars characters Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."},
+        {name: "Dr Nut", description: "Website for Dr Nut drink from the New Orleans based World Bottling Company characters Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."},
+        {name: "Barranquilla Carnival", description: "The following game represents the most popular carnival in Colombia, named BARRANQUILLA'S CARNIVAL, which is one of the most popular traditions, matching cultural and folkloric celebrations. This carnival dates back to the colonial period when the Spanish conquest introduced Spanish and Portuguese traditions in America (Colombia Travel) During this carnival, the most important song is named 'La canción del Carnival' or the carnival's song played by Checo Acosta that you're going to be able to listen, also this drag and drop game was build with HTML, CSS and Javascript."}
+    ];
+
+    console.log(projects)
 
 })();
